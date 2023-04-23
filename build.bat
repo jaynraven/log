eab3cdaea5a97245ba510395ed76ff0b0978b8b2
Linjay7 <1745645904@qq.com>
2023-04-23 22:38:21 +0800

    [modify] 完善cmake配置

---
+++ build.bat
@@ -0,0 +1,17 @@
+rmdir /S /Q build
+rmdir /S /Q install
+
+mkdir build\x86
+mkdir build\x64
+
+cd build\x86
+cmake -A Win32 ..\..
+@REM cmake --build . --config Debug
+cmake --build . --config Release
+cmake --install .
+
+@REM cd ..\x64
+@REM cmake -A x64 ..\..
+@REM cmake --build . --config Debug
+@REM cmake --build . --config Release
+@REM cmake --install .
